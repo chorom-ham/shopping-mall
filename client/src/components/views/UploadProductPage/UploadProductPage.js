3b8b59d652ceb8fa58fbfe4706d1460396e6dc1f
chorom-ham <chorom.m.ham@gmail.com>
2020-07-28 17:46:06 +0900

    Add style & onChange functions in update product page

--- client/src/components/views/UploadProductPage/UploadProductPage.js
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -1,7 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+import { Typography, Button, Form, message, Input, Icon } from "antd";
+
+const { Title } = Typography;
+const { TextArea } = Input;
 
 function UploadProductPage() {
-  return <div>UploadProductPage</div>;
+  const [TitleValue, setTitleValue] = useState("");
+  const [Description, setDescription] = useState("");
+  const [Price, setPrice] = useState(0);
+  const [Continent, setContinent] = useState(1);
+  const [Images, setImages] = useState([]);
+
+  const titleChangeHandler = (e) => {
+    setTitleValue(e.currentTarget.value);
+  };
+
+  return (
+    <div style={{ maxWidth: "700px", margin: "2rem auto" }}>
+      <div style={{ textAlign: "center", marginBottom: "2rem" }}>
+        <Title level={2}>여행 상품 업로드</Title>
+      </div>
+      <Form>
+        {/* DropZone */}
+
+        <br />
+        <br />
+        <label>Title</label>
+        <Input value={TitleValue} onChange={titleChangeHandler} />
+        <br />
+        <br />
+        <label>Description</label>
+        <TextArea />
+        <br />
+        <br />
+        <label>Price($)</label>
+        <Input type="number" />
+        <br />
+        <br />
+        <select>
+          <option></option>
+        </select>
+        <br />
+        <br />
+        <Button>Submit</Button>
+      </Form>
+    </div>
+  );
 }
 
 export default UploadProductPage;
