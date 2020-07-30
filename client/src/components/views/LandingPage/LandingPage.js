ee4837bee7334743b8852467856c431a27869403
chorom-ham <chorom.m.ham@gmail.com>
2020-07-30 18:58:11 +0900

    Get products data from DB

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,16 +1,18 @@
-import React from 'react'
+import React, { useEffect } from "react";
 import { FaCode } from "react-icons/fa";
+import axios from "axios";
 
 function LandingPage() {
-    return (
-        <>
-            <div className="app">
-                <FaCode style={{ fontSize: '4rem' }} /><br />
-                <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
-            </div>
-            <div style={{ float: 'right' }}>Thanks For Using This Boiler Plate by John Ahn</div>
-        </>
-    )
+  useEffect(() => {
+    axios.post("/api/product/products").then((res) => {
+      if (res.data.success) {
+        console.log(res.data);
+      } else {
+        alert("상품 목록을 가져오는 데 실패했습니다.");
+      }
+    });
+  });
+  return <div></div>;
 }
 
-export default LandingPage
+export default LandingPage;
--- server/routes/product.js
+++ server/routes/product.js
@@ -44,4 +44,20 @@ router.post("/", (req, res) => {
   });
 });
 
+//product collection의 모든 상품 정보 가져오기
+router.post("/products", (req, res) => {
+  //product collection안의 모든 정보 찾는 메소드(원하는 조건은 파라미터로)
+  Product.find()
+    .populate("writer")
+    .exec((err, productInfo) => {
+      if (err) {
+        return res.status(400).json({ success: false, err });
+      }
+      return res.status(200).json({
+        success: true,
+        productInfo,
+      });
+    });
+});
+
 module.exports = router;
